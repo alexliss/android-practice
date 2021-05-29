74623ce8094f6a49cff99a0798d48bbd4527b9e6
Alexandra Gridina <sashagridina@gmail.com>
2021-05-29 20:42:34 +0600

    Landscape layout + some work with resources

--- HelloWorld/app/src/main/java/com/redspot/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/redspot/helloworld/MainActivity.java
@@ -2,6 +2,7 @@ package com.redspot.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView greeting = (TextView) findViewById(R.id.greeting);
+        String hello = getResources().getString(R.string.hello_guys);
+        hello += "! ;з";
+        greeting.setText(hello);
     }
 }
