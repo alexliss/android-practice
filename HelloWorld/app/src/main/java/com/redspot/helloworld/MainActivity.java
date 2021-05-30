1e9fb1c5a60193ca3b25e7d7cf5d303c1b003bdc
Alexandra Gridina <sashagridina@gmail.com>
2021-05-31 03:23:44 +0600

    New layout + switch to it! (but for now you can't switch back)

--- HelloWorld/app/src/main/java/com/redspot/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/redspot/helloworld/MainActivity.java
@@ -2,7 +2,10 @@ package com.redspot.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,10 +13,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button refresh = findViewById(R.id.refresh);
+        Button changeCity = findViewById(R.id.changeCity);
+        //Button returnOnMain = findViewById(R.id.editCityDone);
 
-        final TextView greeting = (TextView) findViewById(R.id.temperature);
-        String hello = getResources().getString(R.string.temperature);
-        hello += "! ;з";
-        greeting.setText(hello);
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "meow", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        changeCity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.change_city);
+            }
+        });
+
+
+//        returnOnMain.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                setContentView(R.layout.activity_main);
+//            }
+//        });
+
+        final TextView temperature = (TextView) findViewById(R.id.temperature);
+        String tempValue = getResources().getString(R.string.temperature);
+        tempValue += "! ;з";
+        temperature.setText(tempValue);
+
     }
+
+
 }
